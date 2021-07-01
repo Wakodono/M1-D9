48d450fc71b341e01561392d6a2e39d2afe0a282
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-07-01 14:34:38 +0100

    Steps and heading

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+        BINGO CHALLENGE!!!
+        Create programmatically (with JS) a BINGO board with 76 cells, from 1 to 76.
+        Create a button to randomize a number . The same number should be highlighted on the bingo board
+
+        EXTRA: 
+        - Take ALWAYS a new number (eg. avoid randoming the number 10 3 times)
+        - Create a USER BOARD with 24 randomized numbers that highlights as the main board does
+        - Let the user choose HOW MANY user boards he's willing to play with and create them.
+    */
+
+// Title of application
+
+<h1>BINGO</h1>
+
+// Programmatially create 76 cells, from 1 to 76 with a function (createElement)
+
+
+
+//Button to randomize number from 1 to 76
+
+/* Highlight the button on the bingo board (might have to use some basic css on this one). 
+Try background colour, border style solid 3px, border color white for example*/
