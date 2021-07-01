cdc66c5086066057ad13fd16e80c38ee186df700
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-07-01 15:52:26 +0100

    Tinkered with some functions
    Working on functions to select and highlight a random cell after button click

--- index.js
+++ index.js
@@ -18,9 +18,9 @@
 
 const createBingoBoard = function(){
     for (let BingoCell = 1; BingoCell <=76; BingoCell++){
-        let createBingoCells = document.createElement("div")
-        createBingoCells.innerText = BingoCell
-        BingoCell.classList.add("cell")
+        let createBingoCells = document.createElement("div");
+        createBingoCells.innerText = BingoCell;
+        BingoCell.classList.add("cell");
         
     }
 }
@@ -28,5 +28,28 @@ const createBingoBoard = function(){
 
 //Button to randomize number from 1 to 76
 
+const radomizeNumber = function(){
+    let randomNumberButton = document.createElement("button");
+    randomNumberButton.innerText = "Random"
+    randomNumberButton.onclick = selectCell
+}
+
+const selectCell = function(event){
+    const currentlySelectedCell = getCurrentlySelectedCell() // Looking for any "selected" cell on Bingo Board
+    if (currentlySelectedCell !== null) { // If there is any selected Cell...
+        currentlySelectedCell.classList.remove("selected") // ...we remove the "selected" class from it
+}
+
+// SELECT DAY CELL THAT HAS BEEN CHOSEN AT RANDOM
+const chosenCell = event.target.tagName()  // This give us the HTML element that has been clicked
+clickedDayNode.classList.add("selected")    // We apply "selected" class to it
+
+
 /* Highlight the button on the bingo board (might have to use some basic css on this one). 
-Try background colour, border style solid 3px, border color white for example*/
+Try background colour, border style solid 3px, border color white for example */}
+
+// make sure all functions play out on load with window onload function
+
+window.onload = function(){
+    
+}
